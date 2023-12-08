ba1b62d9e1c1f2a2cd4b1e1202513cc23dc67b68
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-12-08 23:14:04 +0100

    Create task-1-babel.js

---
+++ js/task-1-babel.js
@@ -0,0 +1,48 @@
+const colorPalette = document.querySelector(".color-palette");
+
+const output = document.querySelector(".output");
+
+colorPalette.addEventListener("click", selectColor);
+
+//
+
+function selectColor(event) {
+    if (event.target.nodeName !== "BUTTON") {
+        alert`Oops... Sorry. Try again`;  
+    }
+
+    const selectedColor = event.target.dataset.color;
+    output.textContent = `Selected color: ${selectedColor}`;
+    output.style.color = selectedColor;
+}
+//
+createPaletteItems();
+
+function createPaletteItems() {
+    const items = [];
+    for (let i = 0; i < 60; i++) {
+        const color = getRandomHexColor();
+        const item = document.createElement("button");
+        item.type = "button";
+        item.dataset.color = color;
+        item.style.backgroundColor = color;
+        item.classList.add("item");
+        item.style.border = "1px solid #808080";
+        items.push(item);
+    }
+    colorPalette.append(...items);
+}
+
+function getRandomHexColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+
+
